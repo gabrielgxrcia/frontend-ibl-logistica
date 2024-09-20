5411282b4991da913587fe741482832f050c60e2
Gabriel Garcia <gabrielgrazeffi123@gmail.com>
2024-09-19 23:55:20 -0300

    feat(home): create home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import BookList from '../components/BookList/BookList';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <BookList />
+      
+    </div>
+  );
+};
+
+export default Home;
