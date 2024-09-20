0ed40af890a30d65b8b85453dce5a53d3ed2db70
Gabriel Garcia <gabrielgrazeffi123@gmail.com>
2024-09-20 04:51:02 -0300

    refactor(App): improve component organization and code structure

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,17 @@ import { SearchProvider, useSearchContext } from './contexts/SearchContext';
 import "./styles/global.css";
 
 function AppContent() {
+
+  // Estados para controlar a abertura e fechamento de modais e barra lateral
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [selectedBook, setSelectedBook] = useState(null);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [books, setBooks] = useState([]);
   const navigate = useNavigate();
   const { setSearchResults, setSearchQuery } = useSearchContext();
-  const [books, setBooks] = useState([]);
 
+  // Função para buscar livros
   const fetchBooks = useCallback(async () => {
     try {
       const response = await getBooks();
@@ -31,19 +34,23 @@ function AppContent() {
     fetchBooks();
   }, [fetchBooks]);
 
+  // Funções para abrir e fechar modais e barra lateral
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
-
   const handleOpenEditModal = () => setIsEditModalOpen(true);
   const handleCloseEditModal = () => setIsEditModalOpen(false);
+  const handleCloseSidebar = () => {
+    setSelectedBook(null);
+    setIsSidebarOpen(false);
+  };
 
+  // Função para adicionar um livro
   const handleAddBook = async (bookData) => {
     try {
       const formData = new FormData();
       Object.entries(bookData).forEach(([key, value]) => {
         formData.append(key, value);
       });
-      
       await addBook(formData);
       handleCloseModal();
       await fetchBooks();
@@ -53,6 +60,7 @@ function AppContent() {
     }
   };
 
+  // Função para atualizar um livro
   const handleUpdateBook = async (bookData) => {
     try {
       await updateBook(selectedBook.id, bookData);
@@ -65,6 +73,7 @@ function AppContent() {
     }
   };
 
+  // Função para buscar livros
   const handleSearch = async (query) => {
     setSearchQuery(query);
     if (query.trim()) {
@@ -84,20 +93,18 @@ function AppContent() {
     }
   };
 
+  // Função para abrir a barra lateral com detalhes do livro
   const handleBookClick = (book) => {
     setSelectedBook(book);
     setIsSidebarOpen(true);
   };
 
-  const handleCloseSidebar = () => {
-    setSelectedBook(null);
-    setIsSidebarOpen(false);
-  };
-
+  // Função para abrir o modal de edição
   const handleEditBook = () => {
     handleOpenEditModal();
   };
 
+  // Função para deletar um livro
   const handleDeleteBook = async (id) => {
     try {
       await deleteBook(id);
